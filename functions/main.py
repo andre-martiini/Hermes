d8b7ffc575e78a0aec2a993e30ebfe87ec08ba81
andre-martiini <andre.martiini@gmail.com>
2026-04-18 07:53:43 -0300

    Ajuste CORS do form google

--- functions/main.py
+++ functions/main.py
@@ -4939,7 +4939,6 @@ def criar_apresentacao_slides(req: https_fn.CallableRequest):
 
 
 @https_fn.on_call(
-    cors=options.CorsOptions(cors_origins="*", cors_methods=["POST", "OPTIONS"]),
     memory=options.MemoryOption.MB_512,
     timeout_sec=120,
 )
